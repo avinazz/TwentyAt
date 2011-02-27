4451f12abb9efcbce4a672ebeb433f1fb5c5b652
bipinsutariya <bipin@techingen.com>
2011-02-27 21:28:20 +0530

    exception class change

---
+++ twentyat-dataservice/src/main/java/com/twentyat/exception/TwentyAtProviderException.java
@@ -0,0 +1,25 @@
+package com.twentyat.exception;
+
+public class TwentyAtProviderException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public TwentyAtProviderException() {
+		super();
+	}
+
+	public TwentyAtProviderException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public TwentyAtProviderException(String message) {
+		super(message);
+	}
+
+	public TwentyAtProviderException(Throwable cause) {
+		super(cause);
+	}
+
+}
